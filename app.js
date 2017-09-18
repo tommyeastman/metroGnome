9b6d2bfe53918597f3492c5b0bd8f19e24b2daad
tommyeastman1 <tommyeastman1@gmail.com>
2017-09-18 00:32:19 -0300

    rename variables and re-organize code. same function.

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const sound = document.querySelector("#kick");
 const gnome = document.querySelector(".gnome");
 var BPM = document.querySelector(".bpmSlider input");
-var refreshIntervalId = 0;
-var gnomeIntervalId = 0;
+var oneBeatLoopId = 0;
+var quarterBeatLoopId = 0;
 var isPressed = 0;
 var playToggle = 0;
 
-//for quarter beats
-function play(){
-  sound.play();
-  gnome.classList.add("quarterBeats");
+//for 1 beats
+function oneBeats(){
+  gnome.classList.add("gnomeBig");
 }
 
-//for 1 beats
-function gnomeBig(){
-  gnome.classList.add("playing");
+//for quarter beats
+function quarterBeats(){
+  sound.play();
+  gnome.classList.add("gnomeBlink");
 }
 
 function removeTransition(e){
-  this.classList.remove("playing");
-  this.classList.remove("quarterBeats");
+  this.classList.remove("gnomeBig");
+  this.classList.remove("gnomeBlink");
 }
 
 //Play and stop based on spacebar.
@@ -28,32 +28,32 @@ function spaceBar(e){
   if(`${e.keyCode}`== 32){isPressed += 1;}
   playToggle = isPressed % 2;
   //always clear current loop and set HTML to current BPM value.
-  if(refreshIntervalId !=0){clearInterval(refreshIntervalId)};
-  if(gnomeIntervalId !=0){clearInterval(gnomeIntervalId)};
+  if(oneBeatLoopId !=0){clearInterval(oneBeatLoopId)};
+  if(quarterBeatLoopId !=0){clearInterval(quarterBeatLoopId)};
   document.querySelector("#bpmValue").innerHTML = BPM.value;
   //if play state is 1 then start a new loop
   if(playToggle==1){
-    refreshIntervalId = setInterval(play, 1000*60/BPM.value);
-    gnomeIntervalId = setInterval(gnomeBig, 1000*60/BPM.value*4);
+    quarterBeatLoopId = setInterval(oneBeats, 1000*60/BPM.value*4);
+    oneBeatLoopId = setInterval(quarterBeats, 1000*60/BPM.value);
   }
 }
 
-function handleUpdate(){
+function sliderUpdate(){
   //clear current loop
-  if(refreshIntervalId !=0){clearInterval(refreshIntervalId)};
-  if(gnomeIntervalId !=0){clearInterval(gnomeIntervalId)};
+  if(oneBeatLoopId !=0){clearInterval(oneBeatLoopId)};
+  if(quarterBeatLoopId !=0){clearInterval(quarterBeatLoopId)};
   //set BPM value from the slider.
   BPM.value = this.value;
   //print value to screen.
   document.querySelector("#bpmValue").innerHTML = BPM.value;
   //start new loop at current BPM
-  refreshIntervalId = setInterval(play, 1000*60/BPM.value);
-  gnomeIntervalId =setInterval(gnomeBig, 1000*60/BPM.value*4);
+  quarterBeatLoopId =setInterval(oneBeats, 1000*60/BPM.value*4);
+  oneBeatLoopId = setInterval(quarterBeats, 1000*60/BPM.value);
 }
 
 //trigger spaceBar function from any keydown event.
 window.addEventListener('keydown', spaceBar);
-//trigger handleUpdate function based on any change to slider.
-BPM.addEventListener("change", handleUpdate);
+//trigger sliderUpdate function based on any change to slider.
+BPM.addEventListener("change", sliderUpdate);
 //remove transition
 gnome.addEventListener('transitionend', removeTransition);
